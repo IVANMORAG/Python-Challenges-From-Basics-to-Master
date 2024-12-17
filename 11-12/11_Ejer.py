db43101725ba564cc89fa6c7350b86cb2f585c88
IVANMORAG <ivanmoragarcia412@gmail.com>
2024-12-17 10:50:47 -0600

    Ejercicio 11-12

---
+++ 11-12/11_Ejer.py
@@ -0,0 +1,12 @@
+"""
+EJERCICIO 11:
+Calcula el área de un rectángulo, 
+pide la base y altura al usuario.
+"""
+
+base = input("Ingrese la base: ")
+altura = int(input("Ingrese la altura: "))
+
+base = int(base)
+
+print(f'El área del rectangulo es: {base * altura}')
