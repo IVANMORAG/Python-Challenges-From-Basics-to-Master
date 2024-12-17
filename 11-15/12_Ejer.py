730c036ba5fcea802931cb42312d193ec8ba0c01
IVANMORAG <ivanmoragarcia412@gmail.com>
2024-12-17 10:52:39 -0600

    Ejercicio 11-12

---
+++ 11-15/12_Ejer.py
@@ -0,0 +1,11 @@
+"""
+EJERCICIO 12:
+Convierte un número entero a 
+una cadena de texto.
+"""
+
+numero = 42
+print(f'Tipo de dato del entero: {type(numero)}')
+
+cadena = str(numero)
+print(f"Ahora lo pasamos a cadena con 'str() -> '{type(cadena)}")
