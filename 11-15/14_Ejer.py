f8dd518ffc95115da178c123c22a8239a4d224ee
IVANMORAG <ivanmoragarcia412@gmail.com>
2024-12-17 11:27:50 -0600

    Ejercicio 14

---
+++ 11-15/14_Ejer.py
@@ -0,0 +1,11 @@
+"""
+EJERCICIO 14:
+Pasa una cadena de mayúsculas 
+a minúsculas.
+"""
+
+mayuscula = "FC BARCELONA"
+minuscula = mayuscula.lower()
+
+print(f"En mayúsculas: {mayuscula}")
+print(f"En minúscula: {minuscula}")
