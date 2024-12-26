c255811ead02d7c59564e98c6d3e1ed1e2ee84af
IVANMORAG <ivanmoragarcia412@gmail.com>
2024-12-26 08:51:58 -0600

    Ejercicio 18

---
+++ 16-20/18_Ejer.py
@@ -0,0 +1,10 @@
+"""
+EJERCICIO 18:
+Convierte un número decimal
+a un número entero.
+"""
+
+decimal = float(input("Ingrese un número flotante: "))
+entero = int(decimal)
+
+print(f"Número entero: {entero}")
