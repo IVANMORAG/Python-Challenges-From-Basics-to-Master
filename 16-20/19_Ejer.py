fff7ed5522d4b9b0d3081a78b344a88343a2b919
IVANMORAG <ivanmoragarcia412@gmail.com>
2024-12-26 08:56:41 -0600

    Ejercicio 19

---
+++ 16-20/19_Ejer.py
@@ -0,0 +1,11 @@
+"""
+EJERCICIO 19:
+Cuenta las ocurrencias de un carácter 
+especifico en una cadena.
+"""
+
+cadena = "Tacos al pastor con guacamole"
+
+conteo = cadena.count("a")
+
+print(f"{conteo}")
