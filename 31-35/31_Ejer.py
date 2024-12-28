785686f2d4db7f146c3f89fd62ef61ba91ab3e9d
IVANMORAG <ivanmoragarcia412@gmail.com>
2024-12-27 18:43:11 -0600

    Ejer 31

--- 31-35/31_Ejer.py
+++ 31-35/31_Ejer.py
@@ -3,3 +3,12 @@ EJERCICIO 31:
 Pide un número y verifica si es 
 positivo, negativo o cero.
 """
+
+numero = int(input("Ingrese un número: "))
+
+if numero > 0:
+    print(f"{numero} es positivo.")
+elif numero == 0:
+    print(f"{numero} es cero. ")
+else:
+    print("El número es negativo.")
