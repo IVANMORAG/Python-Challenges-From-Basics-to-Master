71e11b78ceef42a3b3fd35aac023669a2148d085
IVANMORAG <ivanmoragarcia412@gmail.com>
2024-12-27 18:53:24 -0600

    Ejer 33

--- 31-35/33_Ejer.py
+++ 31-35/33_Ejer.py
@@ -6,3 +6,10 @@ Regla de Negocio:
     - No divisible por 100.
     - Divisible por 400.
 """
+
+anio = int(input("Ingrese un año: "))
+
+if (anio % 4 == 0 and anio % 100 != 0) or (anio % 100 == 0):
+    print("Es año biciesto") 
+else:
+    print("NO es")
