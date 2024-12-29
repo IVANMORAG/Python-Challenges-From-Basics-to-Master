ee03085b83d03ef9dadb6eba5a7995c63cc21ae2
IVANMORAG <ivanmoragarcia412@gmail.com>
2024-12-28 19:32:59 -0600

    Ejer 37

--- 36-40/40_Ejer.py
+++ 36-40/40_Ejer.py
@@ -3,3 +3,12 @@ EJERCICIO 40:
 Calcular el IMC e 
 interpretarlo.
 """
+
+a, b, c = input("Ingrese tres números: ").split()
+
+a , b, c = int(a, b, c)
+
+if a > b and a > c:
+    print(f"{a} es el mayor.")
+elif b > a and b > c:
+    print(f"{b} es el mayor")
