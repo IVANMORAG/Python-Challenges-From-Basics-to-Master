0ffb8dd5b65fc591762485af668c7a952a9165b2
IVANMORAG <ivanmoragarcia412@gmail.com>
2024-12-28 19:34:39 -0600

    Ejer 37

--- 36-40/37_Ejer.py
+++ 36-40/37_Ejer.py
@@ -2,3 +2,15 @@
 EJERCICIO 37:
 Calcula el máximo de 3 números.
 """
+
+
+a, b, c = input("Ingrese tres números: ").split()
+
+a , b, c = int(a, b, c)
+
+if a > b and a > c:
+    print(f"{a} es el mayor.")
+elif b > a and b > c:
+    print(f"{b} es el mayor")
+else:
+    print(f"{c} es el mayor.")
