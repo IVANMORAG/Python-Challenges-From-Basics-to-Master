8337c49552a789cf9d9b56ab802252317f36d6a6
IVANMORAG <ivanmoragarcia412@gmail.com>
2024-12-28 19:30:05 -0600

    Ejer 36

--- 36-40/36_Ejer.py
+++ 36-40/36_Ejer.py
@@ -3,3 +3,13 @@ EJERCICIO 36:
 Pide un carácter y determina
 si es una vocal.
 """
+
+caracter = input("Ingrese un caracter: ")
+
+vocales = ['a', 'e', 'i', 'o', 'u']
+
+if caracter.lower() in vocales:
+    print("Es una vocal")
+else:
+    print("NO es una vocal")
+
