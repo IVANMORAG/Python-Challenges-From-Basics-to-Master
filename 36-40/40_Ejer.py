ef38dd42c5f8797fbce796e351b1f7953c22938a
IVANMORAG <ivanmoragarcia412@gmail.com>
2024-12-28 19:58:21 -0600

    Ejer 40

--- 36-40/40_Ejer.py
+++ 36-40/40_Ejer.py
@@ -4,11 +4,22 @@ Calcular el IMC e
 interpretarlo.
 """
 
-
-
-peso = 68
+peso = 60
 estatura = 1.65
 
 imc = peso / (estatura * estatura)
 
 print(imc)
+
+if imc < 18.5:
+    print('Bajo de peso')
+elif imc < 25:
+    print('Peso normal')
+elif imc < 30:
+    print('Sobrepeso')
+elif imc < 35:
+    print('Obesidad grado I')
+elif imc < 40:
+    print('Obesidad grado II')
+else:
+    print('Obesidad grado III')
