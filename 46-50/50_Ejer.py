5625246d1f7a3497b682afe1dcd59d9c42df1ecd
IVANMORAG <ivanmoragarcia412@gmail.com>
2024-12-30 14:30:26 -0600

    Ejer 50

--- 46-50/50_Ejer.py
+++ 46-50/50_Ejer.py
@@ -4,3 +4,16 @@ Mostrar los números del 1 al 100,
 pero reeemplazando los múltiplos de 3 por 
 'Fizz' y los múltiplos de 5 por "Buzz"
 """
+
+numero = 1
+
+for i in range(1, 101):
+    if numero % 3 == 0 and numero % 5 == 0:
+        print("FizzBuzz")
+    elif numero % 3 == 0:
+        print("Fizz")
+    elif numero % 5 == 0:
+        print("Buzz")
+    else:
+        print(numero)
+    numero = numero + 1
