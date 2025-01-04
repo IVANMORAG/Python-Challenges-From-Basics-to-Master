e2d473412be68840019ed743589dfd0924d4193e
IVANMORAG <ivanmoragarcia412@gmail.com>
2025-01-04 14:35:34 -0600

    Ejercicio 69

--- 61-70/69_Ejer.py
+++ 61-70/69_Ejer.py
@@ -5,3 +5,9 @@ la tasa de desempleo
 
 td = no_desempleados / fuerza_laboral X 100
 """
+
+def desempleo(no_em, si_em):
+    return (no_em / si_em) * 100
+
+resultado = desempleo(100, 900)
+print(resultado)
