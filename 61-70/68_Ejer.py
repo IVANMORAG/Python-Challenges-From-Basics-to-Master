90f7107b971c9cf6922dee5eea00b48dc3643512
IVANMORAG <ivanmoragarcia412@gmail.com>
2025-01-04 14:17:26 -0600

    Ejercicio 68

--- 61-70/68_Ejer.py
+++ 61-70/68_Ejer.py
@@ -4,3 +4,8 @@ Escribe una función que pida por teclado
 la distancia y la velocidad para
 poder calcular el tiempo de viaje.
 """
+
+def tiempo(distancia, velocidad):
+    return distancia / velocidad
+
+print(f"El tiempo es: {tiempo(225, 45)}")
