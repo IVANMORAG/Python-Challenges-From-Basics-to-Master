c11411460d04596e810e3b96438cbc862cc39198
IVANMORAG <ivanmoragarcia412@gmail.com>
2025-01-04 13:47:13 -0600

    Ejercicio 65

--- 61-70/64_Ejer.py
+++ 61-70/64_Ejer.py
@@ -3,3 +3,11 @@ EJERCICIO 64:
 Escribe una función para verificar 
 si un número es par o impar
 """
+
+def par_impar(num):
+    if num % 2 == 0:
+        print("Es par")
+    else:
+        print("Es impar")
+        
+par_impar(6)
--- 61-70/65_Ejer.py
+++ 61-70/65_Ejer.py
@@ -3,3 +3,10 @@ EJERCICIO 65:
 Escribe una función para convertir 
 grados celsius a fahrenheit.
 """
+
+def grados(celsius):
+    return ((celsius * 9) / 5) + 32
+
+faht = grados(40)
+
+print(faht)
