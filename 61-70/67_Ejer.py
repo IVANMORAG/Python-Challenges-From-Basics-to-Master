9aa3b5690cfd872b9b6303924a7c5bfb0b18438e
IVANMORAG <ivanmoragarcia412@gmail.com>
2025-01-04 14:11:54 -0600

    Ejercicio 67

--- 61-70/67_Ejer.py
+++ 61-70/67_Ejer.py
@@ -2,5 +2,13 @@
 EJERCICIO 67:
 Escribe una función para calcular 
 el volumen de un cilindro
-v = pi^2h
+v = pir^2h
 """
+
+PI = 3.1416
+
+def cilindro(radio, altura):
+    return PI * radio**2 * altura
+
+resultado = cilindro(3, 5)
+print(resultado)
