d68acb1e71da0e16616a913571f82c20a9476588
IVANMORAG <ivanmoragarcia412@gmail.com>
2025-01-05 01:31:25 -0600

    Ejercicio 78

---
+++ 71-80/78_Ejer.py
@@ -0,0 +1,9 @@
+"""
+EJERCICIO 78:
+Crear una clase persona y otra clase estudiante.
+La clase persona tiene el atributo nombre y 
+el metodo mostrar_nombre()
+La clase estudiante debe heredar de la clase persona 
+y utilizar el metodo mostrar_nombre()
+de la clsae persona
+"""
