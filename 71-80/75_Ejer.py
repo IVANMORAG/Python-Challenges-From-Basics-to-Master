98bf2df9b0bb9949b5c69c71afb99703b853895d
IVANMORAG <ivanmoragarcia412@gmail.com>
2025-01-05 01:23:55 -0600

    Ejercicio 745

---
+++ 71-80/75_Ejer.py
@@ -0,0 +1,9 @@
+"""
+EJERCICIO 75:
+Crear una clase coche con los atributos:
+marca, modelo, matricula, km
+Con los metodos:
+init como constructor
+avanzar(km) este aumenta
+el valor de km en la cantidad.
+"""
