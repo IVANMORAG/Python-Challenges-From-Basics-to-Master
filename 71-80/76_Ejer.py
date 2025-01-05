7e1291e769c929bcabe6e325ead62c8ab2d336ab
IVANMORAG <ivanmoragarcia412@gmail.com>
2025-01-05 01:26:21 -0600

    Ejercicio 76

---
+++ 71-80/76_Ejer.py
@@ -0,0 +1,10 @@
+"""
+EJERCICIO 76:
+Crea una clase animal con los atributos:
+especie y nombre.
+La clase debe tener los metodos:
+init y hablar()
+el metodo hablar() nos retorna una palabra 
+segun la interpretacion del sonido como un perro
+seria "guau"
+"""
