36bce3ca697c27592694fe7106e4d25d9cc72ae9
IVANMORAG <ivanmoragarcia412@gmail.com>
2025-01-05 01:18:36 -0600

    Ejercicio 72

---
+++ 71-80/72_Ejer.py
@@ -0,0 +1,11 @@
+"""
+EJERCICIO 72:
+Crear una clase circulo con los siguientes
+atributos:
+** radio: radio del circulo.
+La clase debe tener los siguientes metodos:
+* __init__(self, radio): Inicializa los atributos
+de la clase.
+* calcular_area(self): calcula y devuelve el area del circulo.
+* calcular_perimetro(self): calcula y devuelve el perimetro del circulo
+"""
