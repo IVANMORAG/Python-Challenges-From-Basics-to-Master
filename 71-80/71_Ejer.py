d1f4b2618b9d953063385b09df32b5d4629355fe
IVANMORAG <ivanmoragarcia412@gmail.com>
2025-01-06 01:17:17 -0600

    Ejer 71

--- 71-80/71_Ejer.py
+++ 71-80/71_Ejer.py
@@ -1,13 +1,5 @@
 """
 EJERCICIO 71:
-Crea una clase Rectangulo con los siguientes
-atributos:
-base: base del rectangulo
-La clase debe de tener los siguientes métodos:
-** __init__(self, base, altura): Inicializa
-los atributos de la clase
-** Calcular_area(self): calcula y devuelve el 
-area del rectangulo.
-** Calcular_perimetro(self): calcula y devuelve 
-el perimetro del rectangulo.
+Crea una función que convierta 
+un texto a mayúsculas.
 """
