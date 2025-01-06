7f3722ca36d9b444ce79e0743d11f24798233afb
IVANMORAG <ivanmoragarcia412@gmail.com>
2025-01-06 01:31:12 -0600

    Ejer 78

--- 71-80/78_Ejer.py
+++ 71-80/78_Ejer.py
@@ -1,9 +1,5 @@
 """
 EJERCICIO 78:
-Crear una clase persona y otra clase estudiante.
-La clase persona tiene el atributo nombre y 
-el metodo mostrar_nombre()
-La clase estudiante debe heredar de la clase persona 
-y utilizar el metodo mostrar_nombre()
-de la clsae persona
+Convierte segundos, horas, minutos y segundos
+usando operaciones matematicas básicas.
 """
