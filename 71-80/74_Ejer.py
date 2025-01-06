13ed7bff24806ecd48005d20be8130f22e1c69cd
IVANMORAG <ivanmoragarcia412@gmail.com>
2025-01-06 01:21:45 -0600

    Ejer 74

--- 71-80/74_Ejer.py
+++ 71-80/74_Ejer.py
@@ -1,9 +1,6 @@
 """
 EJERCICIO 74:
-Crear una clase persona con los atributos:
-** nombre, edad, dni
-con los metodos:
-init()
-es_mayor_de_edad() este retorna True si es mayor 
-de edad
+Calcula cuantos días faltan para 
+terminar el año usando la libreria 
+datatime.
 """
