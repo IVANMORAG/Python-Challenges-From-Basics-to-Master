61c423bbc8a374078df502ec3efe82221442eb54
IVANMORAG <ivanmoragarcia412@gmail.com>
2025-01-06 01:23:37 -0600

    Ejer 76

--- 71-80/76_Ejer.py
+++ 71-80/76_Ejer.py
@@ -1,10 +1,5 @@
 """
 EJERCICIO 76:
-Crea una clase animal con los atributos:
-especie y nombre.
-La clase debe tener los metodos:
-init y hablar()
-el metodo hablar() nos retorna una palabra 
-segun la interpretacion del sonido como un perro
-seria "guau"
+Encuentra las palabras más grandes 
+de un texto usando bucles.
 """
