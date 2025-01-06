6d2f718e4386149b848c7b08090a6f3a18052321
IVANMORAG <ivanmoragarcia412@gmail.com>
2025-01-06 01:32:29 -0600

    Ejer 79

--- 71-80/79_Ejer.py
+++ 71-80/79_Ejer.py
@@ -1,6 +1,5 @@
 """
 EJERCICIO 79:
-Representa una cuenta bancaria con deposito
-y retiro, debe de haber un titular y un 
-saldo, Utiliza POO
+Escribe una función que duplique 
+cada letra de una cadena.
 """
