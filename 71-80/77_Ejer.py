27ac4e80fa6255fdf70a89dfe73840d27ba94a4c
IVANMORAG <ivanmoragarcia412@gmail.com>
2025-01-06 01:24:38 -0600

    Ejer 77

--- 71-80/77_Ejer.py
+++ 71-80/77_Ejer.py
@@ -1,10 +1,5 @@
 """
 EJERCICIO 77:
-Crear una clase llamada persona.
-con los atributos: nombre, edad
-** UN constructor, donde los datos pueden estar vacíos 
-** los setters y getters para cada uno de los atributos.
-** mostrar(): Muestra los datos de la persona.
-** esMayorDeEdad(): Devuelve un valor lógico 
-indicando si es mayor de edad.
+Calcula el volumen de un cilindro usando
+una funcion con parametros.
 """
