21563cbe81d4e0f4812c55df3f0ee7af42b25779
IVANMORAG <ivanmoragarcia412@gmail.com>
2025-01-11 14:29:49 -0600

    Ejer 28

--- 21-30/28_Ejer.py
+++ 21-30/28_Ejer.py
@@ -2,3 +2,7 @@
 EJERCICIO 28:
 Solicita al usuario una lista de números y encuentra el mayor. Usa la función incorporada max()
 """
+
+numeros = list(map(int, input("Ingresa una lista de números separados por espacio: ").split()))
+
+print(f"El número mayor de la lista es {max(numeros)}")
