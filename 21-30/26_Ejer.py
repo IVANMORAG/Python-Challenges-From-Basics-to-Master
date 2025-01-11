1e99955e6e7889a3ba93606d9a32190569311cb7
IVANMORAG <ivanmoragarcia412@gmail.com>
2025-01-11 14:28:10 -0600

    Ejer 26

--- 21-30/26_Ejer.py
+++ 21-30/26_Ejer.py
@@ -4,3 +4,15 @@ Escribe un programa donde el usuario
 adivine un número entre 1 y 10. Usa un 
 bucle while hasta que acierte
 """
+
+import random
+
+numero_secreto = random.randint(1, 10)
+intento = 0
+
+while intento != numero_secreto:
+    intento = int(input("Adivina un número entre 1 y 10: "))
+    if intento == numero_secreto:
+        print("¡Correcto! Adivinaste el número.")
+    else:
+        print("Incorrecto, intenta de nuevo.")
