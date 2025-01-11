32d0bc63ef2c97b73b3536a270c7c59d2388ada0
IVANMORAG <ivanmoragarcia412@gmail.com>
2025-01-11 14:32:09 -0600

    Ejer 30

--- 21-30/30_Ejer.py
+++ 21-30/30_Ejer.py
@@ -3,3 +3,11 @@ EJERCICIO 30:
 Calcula la suma de todos los elementos de una lista usando un bucle for.
 """
 
+numeros = list(map(int, input("Ingresa una lista de números separados por espacio: ").split()))
+
+suma = 0
+
+for num in numeros:
+    suma += num
+
+print(f"La suma de los elementos de la lista es {suma}")
