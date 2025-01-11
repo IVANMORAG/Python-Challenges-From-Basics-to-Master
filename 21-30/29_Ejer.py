d384a75ea759e8b83f912457114514ad9ca28e43
IVANMORAG <ivanmoragarcia412@gmail.com>
2025-01-11 14:26:53 -0600

    Ejer 29

--- 21-30/29_Ejer.py
+++ 21-30/29_Ejer.py
@@ -1,12 +1,5 @@
 """
 EJERCICIO 29:
-Combina dos listas en pares 
-usando la función zip().
+Encuentra el menor de una lista de números usando la función min().
 """
 
-lista1 = [1, 2, 3]
-lista2 = ["a", "b", "c"]
-
-pares = list(zip(lista1, lista2))
-
-print(pares)
