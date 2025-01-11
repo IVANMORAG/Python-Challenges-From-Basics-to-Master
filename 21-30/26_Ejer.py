b8ef2a27c97cd0e935fd5952089a1152a7e2cc04
IVANMORAG <ivanmoragarcia412@gmail.com>
2025-01-11 14:23:35 -0600

    Ejer 26

--- 21-30/26_Ejer.py
+++ 21-30/26_Ejer.py
@@ -1,14 +1,6 @@
 """
 EJERCICIO 26:
-Intercambia los valores  de dos variables
-con asignación múltiple.
+Escribe un programa donde el usuario 
+adivine un número entre 1 y 10. Usa un 
+bucle while hasta que acierte.
 """
-
-a, b = input("Ingrese dos valores para A y B: ").split()
-
-a = int(a)
-b = int(b)
-
-a, b =  b, a
-
-print(f"a: {a}\n b: {b}")
