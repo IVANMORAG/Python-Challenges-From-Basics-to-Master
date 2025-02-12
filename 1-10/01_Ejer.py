eebdde0e1e37a520197f4b012dd711add7fc7a2b
IVANMORAG <ivanmoragarcia412@gmail.com>
2025-02-12 05:59:13 -0600

    Prueba con Arch Linux

--- 1-10/01_Ejer.py
+++ 1-10/01_Ejer.py
@@ -3,7 +3,9 @@ EJERCICIO 1:
 Suma de dos Números y
 mostrar el resultado.
 """
-num1 = int(input("Ingresa un número: "))
-num2 = int(input("Ingresa otro número: "))
+num1 = int(input("Ingrese el primer nùmero: "))
+num2 = int(input("Ingrese el segundo nùmero: "))
+
+resul = num1 + num2
 
 print(f"{num1} + {num2} = {num1 + num2}")
