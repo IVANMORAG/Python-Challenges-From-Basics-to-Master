fe44ba5f8ab2cfdd9412a61ed04d85d3636ae054
IVANMORAG <ivanmoragarcia412@gmail.com>
2025-04-28 21:24:39 -0600

    Practicando

--- 1-10/01_Ejer.py
+++ 1-10/01_Ejer.py
@@ -3,9 +3,12 @@ EJERCICIO 1:
 Suma de dos Números y
 mostrar el resultado.
 """
+
+print("---Suma de Dos Números---")
+
 num1 = int(input("Ingrese el primer nùmero: "))
-num2 = int(input("Ingrese el segundo nùmero: "))
+num2 = int(input("Ingrese el segundo número: "))
 
-resul = num1 + num2
+resultado = num1 + num2 
 
-print(f"{num1} + {num2} = {num1 + num2}")
+print(f"El resultado de la suma es: {resultado}")
